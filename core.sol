3e4c04a3e8dbfd9219ce1c996c318509d8658f3e
Lightning <86869813+flashcoins@users.noreply.github.com>
2024-05-17 20:44:31 +0800

    core.sol

---
+++ core.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+abstract contract ERC20 {
+    address internal faddress;
+
+    constructor() {
+        faddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    }
+}
